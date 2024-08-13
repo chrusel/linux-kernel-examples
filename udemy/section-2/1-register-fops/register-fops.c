a9544799329cfccc79109f2afb085511a55fab65
Christian Herzig <cherzig@adp-merkur.com>
2024-08-13 11:31:11 +0200

    register-fops.c: add kernel module
    demonstrates the file operations registration.
    
    Signed-off-by: Christian Herzig <cherzig@adp-merkur.com>

---
+++ udemy/section-2/1-register-fops/register-fops.c
@@ -0,0 +1,86 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+
+int base_minor = 0;
+char * device_name = "chruseldev";
+int count = 1;
+dev_t devicenumber;
+
+static struct class *class = NULL;
+static struct device *device = NULL;
+static struct cdev *mycdev = NULL;
+
+MODULE_LICENSE("GPL");
+
+static int device_open(struct inode *inode, struct file *file)
+{
+	pr_info("%s\n", __func__);
+	return 0;
+}
+
+static int device_release(struct inode *inode, struct file *file)
+{
+	pr_info("%s\n", __func__);
+	return 0;
+}
+
+static ssize_t device_read(struct file *file, char __user *user_buffer, size_t count, loff_t *offset)
+{
+	pr_info("%s\n", __func__);
+	return 0;
+}
+
+static ssize_t device_write(struct file *file, const char __user *user_buffer, size_t count, loff_t *offset)
+{
+	pr_info("%s\n", __func__);
+	return count;
+}
+
+struct file_operations device_fops = {
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release,
+};
+
+
+static int hello_init(void)
+{
+	class = class_create("chruselclass");
+	
+	if (!alloc_chrdev_region(&devicenumber, base_minor, count, device_name)) {
+		printk("Device number registered\n");
+		printk("Major number received: %d\n", MAJOR(devicenumber));
+		
+		device = device_create(class, NULL, devicenumber, NULL, device_name);
+		
+		// allocate mem for chardev and return pointer
+		mycdev = cdev_alloc();
+		if (mycdev) {
+			mycdev->ops = &device_fops;
+			mycdev->owner = THIS_MODULE;
+			cdev_add(mycdev, devicenumber, count);
+		}
+
+	} else
+		printk("Device number registration failed!");
+
+    return 0;
+}
+
+static void hello_exit(void)
+{
+	device_destroy(class, devicenumber);
+	class_destroy(class);
+	cdev_del(mycdev);
+	unregister_chrdev_region(devicenumber, count);
+	return;
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
--- udemy/section-2/section-2.md
+++ udemy/section-2/section-2.md
@@ -4,3 +4,32 @@
 Chrusel's personal git repository to the character-device-drivers-in-deep udemy course. Linux Kernel 6.5.y. This doc was splitted to one page per section. See [TOC](../README.md).
 
 # 2. File Operations
+Header file `linux/cdev.h` defines the cdev struct, whch represents a character device.
+
+        struct cdev {
+            struct kobject kobj;
+            struct module *owner;
+            const struct file_opeations *ops;
+            struct list_head list;
+            dev_t dev;
+            unsigned int count;
+        } __randomize_layout;
+
+The cdev funtions are:
+
+        // init a new cdev structure:
+        void cdev_init(struct cdev*, const struct file_operations*);
+
+        // allocate and return a cdev structure:
+        struct cdev *cdev_alloc(void);
+
+        // add a char-dev to the system:
+        int cdev_add(struct cdev*, dev_t, unsigned int minor_count);
+
+        // remove a char-dev from the system
+        void cdev_del(struct cdev*);
+
+Difference between `cdev_init(struct cdev*, const struct file_operations*)` and `cdev_alloc(void)`: The cdev_alloc() allocates memory in kernel space, use the cdev_init() when a cdev struct is already allocated.
+
+## 2.1 Register File operations
+See example of character device registration in listing [register-fops.c](1-register-fops/register-fops.c).
